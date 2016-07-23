0baaa5ca46bca0086a2c24a0fe68ccc2d994ab29
Carrycat <Carrycat@bitbucket.org>
2016-07-22 20:32:58 -0400

    firstcommit

---
+++ pogom/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+config = {
+    'LOCALE': 'en',
+    'LOCALES_DIR': 'static/locales',
+    'ROOT_PATH': None,
+    'ORIGINAL_LATITUDE': None,
+    'ORIGINAL_LONGITUDE': None,
+    'GMAPS_KEY': None,
+    'LAST_RECORD_STEP': 0,
+    'LOG_FILE': '',
+}
